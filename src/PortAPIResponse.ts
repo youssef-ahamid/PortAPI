d961e73f208b4a86ff63ebe9b57bd9f3c403a7f2
Youssef <ya1027@nyu.edu>
2023-03-17 15:20:43 +0200

    PortAPIResponse

---
+++ src/PortAPIResponse.ts
@@ -0,0 +1,36 @@
+import { PortAPIError } from './PortAPIError';
+
+export type PortAPISuccessResponse<T> = {
+  success: true;
+  data: T;
+};
+
+export type PortAPIErrorResponse = {
+  success: false;
+  error: PortAPIError;
+};
+
+type Data<T = any> = T extends PortAPIError ? never : T;
+
+export function createPortAPIResponse<T, R extends boolean>(
+  success: R,
+  data: R extends true ? T : PortAPIError,
+): PortAPIResponse<T, R> {
+  if (success) {
+    return {
+      success: true,
+      data: data as Data<T>
+    } as PortAPIResponse<T, R>;
+  }
+
+  return {
+    success: false,
+    error: data
+  } as unknown as PortAPIResponse<T, R>;
+}
+
+export type PortAPIResponse<T, R extends boolean = true> = R extends true
+  ? PortAPISuccessResponse<T>
+  : R extends false
+  ? PortAPIErrorResponse
+  : PortAPISuccessResponse<T> | PortAPIErrorResponse;
